6062248123361ae922a611e44c5f132b3e5f6c30
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-07 19:40:34 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point of the program
  *
- * Description: This function prints a message with proper grammar and returns 0
+ * Description: This function prints a message with proper grammar and
+ *              returns 0
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
 
