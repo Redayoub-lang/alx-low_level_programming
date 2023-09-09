837591e9e49e51f17e14b01fc1d3690bb84d9b78
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-09 16:50:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,17 +10,17 @@
  */
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    char *ptr = message;
-    int len = 0;
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *ptr = message;
+	int len = 0;
 
-    while (*ptr)
-    {
-        len++;
-        ptr++;
-    }
+	while (*ptr)
+	{
+		len++;
+		ptr++;
+	}
 
-    write(STDERR_FILENO, message, len);
-    return (1);
+	write(STDERR_FILENO, message, len);
+	return (1);
 }
 
