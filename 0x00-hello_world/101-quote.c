30f4c8c1540b91acbf96d2808075e8f8c0e82658
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-09 18:21:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,5 @@
-#include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Description: This program writes a specific message to the standard error.
- *
- * Return: Always 1 (indicating an error)
- */
-int main(void)
-{
-	write(STDERR_FILENO,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
-	return (1);
+#include <stdio.h>
+int main() {
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return 1;
 }
