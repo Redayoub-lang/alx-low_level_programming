e609ce5b47bd62e967dcf6fa9c9256c0f4e03c21
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-10 02:03:06 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point
  *
- * Description: This program writes a specific message to the standard error.
+ * Description: This program writes the specified message to the standard error.
  *
  * Return: Always 1 (indicating an error)
  */
 int main(void)
-{char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+{
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, 59);  // Write the message to the standard error
 
-        write(1, quo, 59)
-        return (1);
+    return (1);  // Return 1 to indicate an error
 }
