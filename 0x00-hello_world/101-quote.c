9ebcc95b3a3e0f7cf20820c26546c30cb7e89b8a
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-09 17:07:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,26 +1,11 @@
 #include <unistd.h>
 #include <stdlib.h>
-
 /**
- * main - Entry point
- *
- * Description: This program writes a specific message to the standard error.
- *
- * Return: Always 1 (indicating an error)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	char *ptr = message;
-	int len = 0;
-
-	while (*ptr)
-	{
-		len++;
-		ptr++;
-	}
-
-	write(STDERR_FILENO, message, len);
+	fput( "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", Stdout);
 	return (1);
 }
-
