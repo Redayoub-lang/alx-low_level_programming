4962628a548c1ff62bc6dc97bb71901b89347a44
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-10 01:57:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  * Return: Always 1 (indicating an error)
  */
 int main(void)
-{Char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+{char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 
         write(1, quo, 59)
         return (1);
