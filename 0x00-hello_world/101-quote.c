9bc9ef434c4218df47d43088be0f02adcc3e7e7c
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-10 01:44:44 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,6 +9,8 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	Char quo[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	
+	whrite(1, quo, 59)
 	return (1);
 }
