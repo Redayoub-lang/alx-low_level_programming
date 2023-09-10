0e0b22c65e739e367fef8799257fb56b3bec88d5
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-10 07:03:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
- 
 #include <unistd.h>
 
-/**
+ /**
  * main - Entry point
  *
- * Description: This program writes the specified message to the standard error.
+ * Description:This program writes the specified message to the standard error.
  *
  * Return: Always 1 (indicating an error)
-*/
+ */
 int main(void)
 { char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59); /* Write the message to the standard error */
-
-    return (1); /* Return 1 to indicate an error */
+    write(2,message,59);/* Write the message to the standard error */
+    return(1);/* Return 1 to indicate an error */
 }
 
