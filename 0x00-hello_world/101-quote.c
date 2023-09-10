e1011f20fa2ab817e22c1eadcb6bcc120951bc06
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-10 02:17:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
  * Return: Always 1 (indicating an error)
  */
 int main(void)
-{
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{ char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, 59); /* Write the message to the standard error */
 
     return (1); /* Return 1 to indicate an error */
