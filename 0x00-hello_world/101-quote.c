e2db600539dc4151dbb59bb3a3e73af20f0d0cd4
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-09 18:34:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program writes a specific message to the standard error.
+ *
+ * Return: 1 (Always, indicating an error)
+ */
 int main(void)
 {
- printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
- return 1;
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
