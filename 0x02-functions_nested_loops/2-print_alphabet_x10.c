d018216711fc940b3900f52579a645935ab11235
Ayoub Lahmar <redayoub9@gmail.com>
2023-09-12 12:26:37 -0700

    Write a function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - Prints the lowercase alphabet ten times.
  */
-
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+char letter;
+int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        letter = 'a';
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        _putchar('\n');
-    }
+for (i = 0; i < 10; i++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+	}
 }
 
